36ac4abfd4bb1fa241c99cae4c2161877ed28e5c
Andrew Alderson <andrew.alderson@outlook.com>
2025-09-10 00:12:15 +0000

    chore(material/event-calendar): add storybook

---
+++ packages/material/event-calendar/src/lib/event-calendar.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { MatEventCalendar } from './event-calendar';
+
+const meta: Meta<MatEventCalendar> = {
+  component: MatEventCalendar,
+  title: 'MatEventCalendar',
+};
+export default meta;
+
+type Story = StoryObj<MatEventCalendar>;
+
+export const Primary: Story = {
+  args: {},
+};
